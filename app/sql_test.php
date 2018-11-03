b56b44fecf5fa42cd9a22646c82af3b6154f1397
Brandon Clement <brandon.clement@u-bordeaux.fr>
2018-11-03 10:58:04 +0100

    Add phpMyAdmin functionality; reorganize file structure

--- app/sql_test.php
+++ app/sql_test.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('mariadb', 'test', 'password', 'testdb');
+$mysqli = new mysqli('mariadb', 'cdp25', 'r8T44vN9qQ', 'scrum');
 
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
