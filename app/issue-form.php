167fcf5ba459451a7f5c6f59579ae95004485742
Brandon Clement <brandon.clement@u-bordeaux.fr>
2018-11-08 22:44:17 +0100

    Adds error messages and migrates priority from a string to an int

--- app/issue-form.php
+++ app/issue-form.php
@@ -1,8 +1,12 @@
 <?php
-namespace app;
+/*
+ * Add New Issue: issue-form.php
+ * Modify Issue #3: issue-form.php?id=3
+ */
 
 function getIssue($id) {
-    $mysqli = new \mysqli($_ENV['MYSQL_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], $_ENV['MYSQL_DATABASE']);
+    $mysqli = new mysqli($_ENV['MYSQL_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], $_ENV['MYSQL_DATABASE']);
+
     if ($mysqli->connect_error) {
         $error = 'Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
         trigger_error($error, E_USER_ERROR);
@@ -12,23 +16,35 @@ function getIssue($id) {
     $sql = 'SELECT * FROM issues WHERE id = ? LIMIT 1';
 
     $stmt = $mysqli->prepare($sql);
-    # Assert $stmt !== FALSE
+    if ($stmt === FALSE) {
+        $error = 'Prepare Error (' . $mysqli->errno . ') ' . $mysqli->error;
+        trigger_error($error, E_USER_ERROR);
+        return FALSE;
+    }
     $stmt->bind_param('i', $id);
 
     if (!$stmt->execute()) {
-        # Error
+        $error = 'Execute Error (' . $stmt->errno . ') ' . $stmt->error;
+        trigger_error($error, E_USER_ERROR);
         return FALSE;
     }
 
     $result = $stmt->get_result();
-    # Assert $result !== FALSE
-    return $result->fetch_assoc();
+    if ($stmt === FALSE) {
+        $error = 'Result Error (' . $mysqli->errno . ') ' . $mysqli->error;
+        trigger_error($error, E_USER_ERROR);
+        return FALSE;
+    }
+
+    $issue = $result->fetch_assoc();
+    $mysqli->close();
+    return $issue;
 }
 
 if (isset($_GET['id']) && ctype_digit($_GET['id'])) {
     $action = 'modify';
     $id = $_GET['id'];
-    $issue = getIssue($id);
+    $issue = getIssue(intval($id));
     $TITLE = "Modify Issue #$id";
 } else {
     $action = 'add';
@@ -44,7 +60,7 @@ if (isset($_GET['id']) && ctype_digit($_GET['id'])) {
         <title><?php echo $TITLE; ?></title>
     </head>
     <body>
-        <a href="backlog.php">&#8676; Back to Backlog</a>
+        <a href="backlog.php">&#8676; Backlog</a>
         <h1><?php echo $TITLE; ?></h1>
         <form action="query.php" method="post">
             <input id="action" name="action" type="hidden" value="<?php echo $action; ?>" />
@@ -55,13 +71,11 @@ if (isset($_GET['id']) && ctype_digit($_GET['id'])) {
                     <td>
                         <textarea id="description" name="description"
                                   maxlength="500" rows="5" cols="50"
-                                  required="required">
-                            <?php if (is_array($issue)) echo $issue['description']; ?>
-                        </textarea>
+                                  required="required"><?php if (is_array($issue)) echo $issue['description']; ?></textarea>
                     </td>
                 </tr>
                 <tr>
-                    <td>Realization Cost:</td>
+                    <td>Cout de Réalisation:</td>
                     <td>
                         <?php $cost = is_array($issue) ? $issue['cost'] : 0; ?>
                         <input id="cost" name="cost" type="number" min="0" step="0.5"
@@ -69,18 +83,17 @@ if (isset($_GET['id']) && ctype_digit($_GET['id'])) {
                     </td>
                 </tr>
                 <tr>
-                    <td>Priority:</td>
+                    <td>Priorité:</td>
                     <td>
-                    </td>
                         <?php
-                        $priority = is_array($issue) ? $issue['priority'] : '';
+                        $priority = is_array($issue) ? $issue['priority'] : 0;
                         $selected = 'selected="selected"';
                         ?>
                         <select id="priority" name="priority">
-                            <option value="" <?php if ($priority === '') echo $selected; ?>>None</option>
-                            <option value="low" <?php if ($priority === 'low') echo $selected; ?>>Low</option>
-                            <option value="medium" <?php if ($priority === 'medium') echo $selected; ?>>Medium</option>
-                            <option value="high" <?php if ($priority === 'high') echo $selected; ?>>High</option>
+                            <option value="0" <?php if ($priority === 0) echo $selected; ?>>None</option>
+                            <option value="1" <?php if ($priority === 1) echo $selected; ?>>Low</option>
+                            <option value="2" <?php if ($priority === 2) echo $selected; ?>>Medium</option>
+                            <option value="3" <?php if ($priority === 3) echo $selected; ?>>High</option>
                         </select>
                     </td>
                 </tr>
