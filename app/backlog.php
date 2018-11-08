84842046a286a4fa7f5e8d9653442faa4aaaa7b4
Brandon Clement <brandon.clement@u-bordeaux.fr>
2018-11-08 22:56:46 +0100

    Cleans up PHP code

--- app/backlog.php
+++ app/backlog.php
@@ -11,15 +11,17 @@
     </tr>
     <?php
         $sizeRaw = 4;
-        $backlog[] = array('0','do0','cout0','prio0');
-        $backlog[] = array('1','do1','cout1','prio1');
+        $backlog = array(
+            array('0','do0','cout0','prio0'),
+            array('1','do1','cout1','prio1'),
+        );
 
-        for ($i=0; $i < sizeof($backlog); $i++) {
-            $id = $backlog[$i][0];
+        foreach ($backlog as $issue) {
+            $id = $issue[0];
 
             echo "<tr>";
             for ($j=0; $j < $sizeRaw; $j++) {
-                echo "<td>".$backlog[$i][$j]."</td>";
+                echo "<td>".$issue[$j]."</td>";
             }
             echo '<td><a href="issue-form.php?id=' . $id . '">modify</a></td>';
             echo '<td><a href="#">delete</a></td>';
