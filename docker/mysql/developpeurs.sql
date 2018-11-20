42342d8177e005581ea7f43c5695b3533d4ced1a
LucasVivas <lucas.vivas33@gmail.com>
2018-11-20 12:30:10 +0100

    add developpeurs.sql and add it on mysql server

---
+++ docker/mysql/developpeurs.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `scrum`.`developpeurs` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email` TEXT NOT NULL,
+    `username` TEXT NOT NULL,
+    `password` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+);
