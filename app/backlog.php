a87f756440648aa0836fc06cb72d2325e235237d
Brandon Clement <brandon.clement@u-bordeaux.fr>
2018-11-23 14:30:44 +0100

    Corrects HTML

--- app/backlog.php
+++ app/backlog.php
@@ -1,32 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Backlog</title>
+    </head>
+    <body>
+        <h2>Backlog</h2>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>Description</td>
+                <td>Cout de Réalisation</td>
+                <td>Priorité</td>
+            </tr>
+            <?php
+            $sizeRaw = 4;
+            $backlog = array(
+                array('0','do0','cout0','prio0'),
+                array('1','do1','cout1','prio1'),
+            );
 
- ?>
-<h2>Backlog</h2>
-<table>
-    <tr>
-        <td>ID</td>
-        <td>Description</td>
-        <td>Cout de Réalisation</td>
-        <td>Priorité</td>
-    </tr>
-    <?php
-        $sizeRaw = 4;
-        $backlog = array(
-            array('0','do0','cout0','prio0'),
-            array('1','do1','cout1','prio1'),
-        );
+            foreach ($backlog as $issue) {
+                $id = $issue[0];
 
-        foreach ($backlog as $issue) {
-            $id = $issue[0];
-
-            echo "<tr>";
-            for ($j=0; $j < $sizeRaw; $j++) {
-                echo "<td>".$issue[$j]."</td>";
+                echo "<tr>";
+                for ($j=0; $j < $sizeRaw; $j++) {
+                    echo "<td>".$issue[$j]."</td>";
+                }
+                echo '<td><a href="issue-form.php?id=' . $id . '">modify</a></td>';
+                echo '<td><a href="#">delete</a></td>';
+                echo "</tr>";
             }
-            echo '<td><a href="issue-form.php?id=' . $id . '">modify</a></td>';
-            echo '<td><a href="#">delete</a></td>';
-            echo "</tr>";
-        }
-    ?>
-</table>
-<a href="issue-form.php">Add issue</a>
+            ?>
+        </table>
+        <a href="issue-form.php">Add issue</a>
+    </body>
+</html>
