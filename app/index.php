8d92dba547dd65559f5187c4028bd25c627cd665
Brandon Clement <brandon.clement@u-bordeaux.fr>
2018-11-23 16:34:02 +0100

    Removes auto-incremented id column from insert statement

--- app/index.php
+++ app/index.php
@@ -1,8 +1,8 @@
 <?php include 'query.php';
     $bdd = new PDO('mysql:host=mysql:3306;dbname=scrum', 'root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO Projects (id, name, sprintLenght, description)
-    VALUES ('1', 'test', '8', 'description test')";
+    $sql = "INSERT INTO Projects (name, sprintLenght, description)
+    VALUES ('test', '8', 'description test')";
     $bdd->query($sql);
 ?>
 <?php include 'backlog.php'; ?>
