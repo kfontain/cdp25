176a42d54a848ad8b098a3a7c2e31daef0ebedfb
Brandon Clement <brandon.clement@u-bordeaux.fr>
2018-12-01 11:45:12 +0100

    Improves e2e test launcher script

--- tests/e2e/run-all.sh
+++ tests/e2e/run-all.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-set -e
-for file in *.py; do
-	python "$file"
+
+DIR="$(dirname "$(which "$0")")"
+ERR_LOG=${DIR}/error.log
+
+[[ -f ${ERR_LOG} ]] && rm ${ERR_LOG}
+
+for file in ${DIR}/*.py; do
+    now="$(date)"
+    printf "%s (%s)\n-----------\n" "$file" "$now" >> ${ERR_LOG}
+
+	if python "$file" 2>> ${ERR_LOG}; then
+        result="SUCCESS"
+    else
+        result="FAIL"
+    fi
+
+    printf "%s: %s\n" "$file" "$result"
+
+    printf "\n" >> ${ERR_LOG}
 done
