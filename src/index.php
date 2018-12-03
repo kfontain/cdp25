9452119c39d3cdeb95e1f91e2103dcb16ac70b68
kfontain <fontaken@gmail.com>
2018-12-03 14:42:16 +0100

    Removed commented code

--- src/index.php
+++ src/index.php
@@ -1,8 +1,2 @@
-<?php include 'query.php';
-    // $bdd = new PDO('mysql:host=mysql:3306;dbname=scrum', 'root', '');
-    // $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // $sql = "INSERT INTO Projects (name, sprintLenght, description)
-    // VALUES ('test', '8', 'description test')";
-    // $bdd->query($sql);
-?>
+<?php include 'query.php';?>
 <?php include 'backlog.php'; ?>
