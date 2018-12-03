2b6f9203696598d648e1751887b5da7e6df79884
Kenji Fontaine <kenfontaine@emi.u-bordeaux1.fr>
2018-12-03 14:57:36 +0100

    Remove commented code & unused parameters

--- src/query.php
+++ src/query.php
@@ -1,36 +1,22 @@
 <?php
 
-/*
- * connect()
- * ISSUES
- * getIssues()
- * deleteIssue(id)
- * createIssue(description, cost, prio)
- * modifyIssue(id, description, cost, prio)
- *
- * PROJECTS
- * createProject(name, sprintLenght)
- */
-
 function getIssues()
 {
-    //return $bdd->query('SELECT * FROM `Issues`');
 }
 
-function deleteIssue($id=-1)
+function deleteIssue()
 {
 }
 
-function createIssue($description='', $cost=0, $prio='')
-{
-    //INSERT INTO `issues` (`id`, `description`, `cost`, `priority`) VALUES (NULL, $description, $cost, $prio);
-}
-
-function ModifyIssue($id=-1, $description='', $cost=0, $prio='')
+function createIssue()
 {
 }
 
-function createProject($name=null, $sprintLenght=null, $description="")
+function ModifyIssue()
+{
+}
+
+function createProject()
 {
 }
 
